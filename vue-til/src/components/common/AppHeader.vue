a5a1264e5e6ade1be9e3d30434bd9cd6132ce5ee
chocozzang <jangjh_05@naver.com>
2024-03-27 01:11:40 +0900

    vue js end game 설정

---
+++ vue-til/src/components/common/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		header
+		<demo-vue></demo-vue>
+	</div>
+</template>
+
+<script>
+// import DemoVue from "../../demo/basic/DemoVue.vue";
+import DemoVue from "@/demo/basic/DemoVue.vue";
+
+export default {
+	components: {
+		DemoVue,
+	},
+};
+</script>
+
+<style></style>
