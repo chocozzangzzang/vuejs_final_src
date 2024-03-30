6b35c921977bb171a368d8435beccfd9e0b52815
chocozzang <jangjh_05@naver.com>
2024-03-30 14:24:01 +0900

    로그인 상태 처리 및 로그아웃 구현 240330

--- vue-til/src/components/LoginForm.vue
+++ vue-til/src/components/LoginForm.vue
@@ -1,18 +1,31 @@
 <template>
-	<form @submit.prevent="submitForm">
-		<div>
-			<label for="username">ID : </label>
-			<input id="username" type="text" v-model="username" />
+	<div class="contents">
+		<div class="form-wrapper form-wrapper-sm">
+			<form @submit.prevent="submitForm">
+				<div>
+					<label for="username">ID : </label>
+					<input id="username" type="text" v-model="username" />
+					<p class="validation-text">
+						<span class="warning" v-if="!isUsernameValid && username">
+							Please enter an email address
+						</span>
+					</p>
+				</div>
+				<div>
+					<label for="password">PW : </label>
+					<input id="password" type="text" v-model="password" />
+				</div>
+				<button
+					v-bind:disabled="!isUsernameValid || !password"
+					type="submit"
+					class="btn"
+				>
+					로그인
+				</button>
+				<p class="log">{{ logMessage }}</p>
+			</form>
 		</div>
-		<div>
-			<label for="password">PW : </label>
-			<input id="password" type="text" v-model="password" />
-		</div>
-		<button v-bind:disabled="!isUsernameValid || !password" type="submit">
-			로그인
-		</button>
-		<p>{{ logMessage }}</p>
-	</form>
+	</div>
 </template>
 
 <script>
@@ -41,8 +54,12 @@ export default {
 					password: this.password,
 				};
 				const { data } = await loginUser(loginData);
+				// 메인 페이지로 이동 //
 				console.log(data.user.username);
+				this.$store.commit("setUsername", data.user.username);
 				this.logMessage = `${data.user.username} 님 환영합니다.`;
+				this.$router.push("/main");
+				// < router-link to="" > //
 				// this.initForm();
 			} catch (error) {
 				// 에러 핸들링 코드
@@ -61,4 +78,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.btn {
+	color: white;
+}
+</style>
